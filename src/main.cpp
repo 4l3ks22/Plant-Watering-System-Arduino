3ec94a83764870b9558919fc3fcafdd8f37154aa
4l3ks22 <ak.alexanderkuba22@gmail.com>
2026-04-21 17:30:24 +0200

    Deleted LED pin test

--- src/main.cpp
+++ src/main.cpp
@@ -4,21 +4,19 @@
 
 int relayPin = 12;
 int moistureSensorPin = A2;
-int ledPin = 13;
 
-unsigned long lastCheck = 0;
-const long interval = 4000;
+long lastCheck = 0;
+const long interval = 4000; // interval to check moisture (4 seconds)
 
-// OLED object
+// OLED screen object
 U8G2_SSD1306_128X64_NONAME_F_HW_I2C u8g2(U8G2_R0, U8X8_PIN_NONE);
 
 void setup() {
   Serial.begin(9600);
 
-  pinMode(relayPin, OUTPUT);
-  pinMode(ledPin, OUTPUT);
+  pinMode(relayPin, OUTPUT); 
 
-  digitalWrite(relayPin, HIGH); // Relay OFF
+  digitalWrite(relayPin, HIGH); // Relay OFF from start
   u8g2.begin();
 }
 
@@ -30,7 +28,7 @@ void loop() {
     int sensorRead = analogRead(moistureSensorPin);
     Serial.println(sensorRead);
 
-    // OLED display
+    // OLED display showing moisture level
     u8g2.clearBuffer();
     u8g2.setFont(u8g2_font_ncenB08_tr);
     u8g2.drawStr(20, 20, "Moisture Level:");
@@ -41,10 +39,10 @@ void loop() {
     // Pump control
     if (sensorRead < 300) {
       digitalWrite(relayPin, LOW); // ON
-      digitalWrite(ledPin, HIGH);
+      
     } else {
       digitalWrite(relayPin, HIGH); // OFF
-      digitalWrite(ledPin, LOW);
+      
     }
   }
 }
